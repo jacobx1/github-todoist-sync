c215aff84f943adf4745c876131b1be178b581b7
Jacob Schwind <jacobx1@gmail.com>
2017-03-04 11:05:20 -0500

    Initial commit - GitHub-Todoist basic import working

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace github_todoist_net
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Octokit.GitHubClient m_client = null;
+        Todoist.Net.TodoistClient m_todoistClient = null;
+        Octokit.User m_currentUser = null;
+
+        class RepositoryItem
+        {
+            public long id = 0;
+            public string name;
+            public override string ToString()
+            {
+                return name;
+            }
+        }
+
+        class MilestoneItem
+        {
+            public int number = 0;
+            public string name;
+            public override string ToString()
+            {
+                return name;
+            }
+        }
+
+        class TodoistItem
+        {
+            public Todoist.Net.Models.ComplexId id;
+            public string name;
+
+            public override string ToString()
+            {
+                return name;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GithubToken.Password = Properties.Settings.Default.GithubToken;
+            TodoistToken.Password = Properties.Settings.Default.TodoistToken;
+            GithubURL.Text = Properties.Settings.Default.GithubURL;
+        }
+
+        async void Connect(string githubUrl, string githubToken, string todoistToken)
+        {
+            m_client = new Octokit.GitHubClient(new Octokit.ProductHeaderValue("github-todoist"), new Uri(githubUrl));
+            m_client.Credentials = new Octokit.Credentials(githubToken);
+
+            m_currentUser = await m_client.User.Current();
+
+            GithubUserName.Content = String.Format("Connected: {0}", m_currentUser.Login);
+            GithubUserName.Foreground = Brushes.Green;
+
+            var allRepos = await m_client.Repository.GetAllForCurrent();
+
+            Repository.Items.Clear();
+            foreach (var repo in allRepos)
+            {
+                var repoItem = new RepositoryItem() { name = repo.FullName, id = repo.Id };
+                Repository.Items.Add(repoItem);
+            }
+
+            m_todoistClient = new Todoist.Net.TodoistClient(todoistToken);
+
+            var projects = await m_todoistClient.Projects.GetAsync();
+
+            TodoistProjects.Items.Clear();
+            foreach (var project in projects)
+            {
+                var todoistItem = new TodoistItem() { name = project.Name, id = project.Id };
+                TodoistProjects.Items.Add(todoistItem);
+            }
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.GithubToken = GithubToken.Password;
+            Properties.Settings.Default.TodoistToken = TodoistToken.Password;
+            Properties.Settings.Default.GithubURL = GithubURL.Text;
+
+            Properties.Settings.Default.Save();
+
+            Connect(GithubURL.Text, GithubToken.Password, TodoistToken.Password);
+        }
+
+        private async void Sync_Click(object sender, RoutedEventArgs e)
+        {
+            var milestoneItem = GithubMilestones.SelectedItem as MilestoneItem;
+            var projectItem = TodoistProjects.SelectedItem as TodoistItem;
+            var repoItem = Repository.SelectedItem as RepositoryItem;
+            
+            if (milestoneItem == null || projectItem == null || repoItem == null)
+            {
+                return;
+            }
+
+            var issueRequest = new Octokit.RepositoryIssueRequest() { Milestone = milestoneItem.number.ToString(), Assignee = m_currentUser.Login, State = Octokit.ItemStateFilter.Open };
+            var issues = await m_client.Issue.GetAllForRepository(repoItem.id, issueRequest);
+
+            if (issues == null)
+            {
+                return;
+            }
+
+            SyncProgress.Maximum = issues.Count;
+            SyncProgress.Value = 0;
+
+            foreach (var issue in issues)
+            {
+                var title = String.Format("**{0}** [{1}]({2})", issue.Number, issue.Title, issue.HtmlUrl.ToString());
+
+                var todoistItem = new Todoist.Net.Models.Item(title, projectItem.id);
+                await m_todoistClient.Items.AddAsync(todoistItem);
+
+                ++SyncProgress.Value;
+            }
+        }
+
+        private async void Repository_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GithubMilestones.Items.Clear();
+
+            if (e.AddedItems.Count == 0)
+            {
+                return;
+            }
+
+            var item = e.AddedItems[0] as RepositoryItem;
+            if (item == null)
+            {
+                return;
+            }
+
+            var milestones = await m_client.Issue.Milestone.GetAllForRepository(item.id);
+            foreach (var milestone in milestones)
+            {
+                var milestoneItem = new MilestoneItem() { name = milestone.Title, number = milestone.Number };
+                GithubMilestones.Items.Add(milestoneItem);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# GitHub to Todoist Issue Importer
+
+Import open issues from GitHub milestones to a Todoist Project.
+
+## Setup
+
+1. Generate a GitHub personal access token - https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+	+ Need access to read repo data - issues and milestones
+2. Get your todoist api token
+	+ Todoist > Settings > Account
+3. _(Optional)_ Change GitHub url for Enterprise installations
+
+Settings are saved after each refresh.
+
+## Usage
+
+After setting up the importer and doing a refresh to pull data:
+
+1. Select repository to get issues from
+2. Select milestone for import
+3. Select Todoist project to import issues to
+4. Click `===>` to import from milestone to Todoist Project
